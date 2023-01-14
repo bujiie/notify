acafc9ef8d69a20ade1414cfe82a25e7435cdade
Brian Ujiie <bujiie+github@gmail.com>
2023-01-14 01:09:34 -0800

    Arizmendi monitor.

--- notify.py
+++ notify.py
@@ -3,6 +3,8 @@
 import sys
 import abc
 import re
+import concurrent.futures
+import threading
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
@@ -32,30 +34,35 @@ class HtmlMonitor(abc.ABC):
         url = self.url()
         if not url:
             print("No URL provided.")
-            sys.exit(1)
+            return
 
         response = self.session.get(url)
         if response.status_code != 200:
             print(f"Response from url:{url} was not successful.")
-            sys.exit(1)
+            return
 
         parsed = self.parse(BeautifulSoup(response.content, "html.parser"))
         if not parsed:
             print("Nothing returned from parsing.")
-            sys.exit(1)
+            return
 
         if self.alert_if(parsed):
             message = self.alert_message(parsed)
-            print(f"[ALERT]: {message}")
-        else:
-            print("Nothing to alert.")
-        sys.exit(0)
+            if isinstance(message, list):
+                for m in message:
+                    self.__alert(m)
+            else:
+                self.__alert(message)
+        return
+
+    def __alert(self, message):
+        print(f"{self.__class__.__name__} - [ALERT]: {message}")
 
 
 class StandardFareMonitor(HtmlMonitor):
     def __init__(self, session, keywords=[]):
         super(StandardFareMonitor, self).__init__(session)
-        self.keywords = keywords
+        self.keywords = list(map(lambda k: k.lower(), keywords))
 
     def url(self):
         return "https://www.standardfareberkeley.com/lunch"
@@ -85,9 +92,10 @@ class StandardFareMonitor(HtmlMonitor):
 
             # Check to see if any of the keywords exist in the names of the
             # filtered list of sandwiches.
+            sandwich_item_str = sandwich_item.string.lower()
             for keyword in self.keywords:
-                if keyword in sandwich_item.string.lower():
-                    sandwich_match = (sandwich_item.string, sandwich_desc[0].string)
+                if keyword in sandwich_item_str:
+                    sandwich_match = (sandwich_item_str, sandwich_desc[0].string)
                     break
 
             # Once we have found a sandwich with a matching keyword, we are done
@@ -104,12 +112,89 @@ class StandardFareMonitor(HtmlMonitor):
         }
 
     def alert_if(self, parsed):
-        return parsed["date"] == date.today().strftime("%B %d") and parsed["sandwich"]
+        return True
+        # return parsed["date"] == date.today().strftime("%B %d") and parsed["sandwich"]
 
     def alert_message(self, parsed):
         return f"{parsed['sandwich']} ({parsed['date']}) - {parsed['desc']}"
 
 
+class ArizmendiMonitor(HtmlMonitor):
+    def __init__(self, session, keywords=[]):
+        super(ArizmendiMonitor, self).__init__(session)
+        self.keywords = [[i.lower() for i in ingredients] for ingredients in keywords]
+
+    def url(self):
+        return "https://www.arizmendi-bakery.org/arizmendi-emeryville-pizza"
+
+    def parse(self, html=None):
+        daterange_element = html.find("p", string=re.compile('Pizza Forecast for'))
+        if not daterange_element:
+            return None
+
+        # Assuming date range in format 'Month ##-##'
+        [month, dayrange] = daterange_element.string.split(" ")[-2:]
+        [startday, endday] = dayrange.split("-")
+
+        weekly_menu = {
+            "WEDNESDAY": None,
+            "THURSDAY": None,
+            "FRIDAY": None,
+            "SATURDAY": None,
+            "SUNDAY": None
+        }
+        # Search for the days of the week and their corresponding pizza
+        for day in weekly_menu.keys():
+            menu_element = html.select(f"p:-soup-contains('{day}')")
+            if not menu_element or len(menu_element) < 1:
+                continue
+            pizza = menu_element[0].text.replace(day, "").strip().lower()
+
+            keyword_match = False
+            for keyword in self.keywords:
+                keyword_inner_match = True
+                for kw in keyword:
+                    if kw not in pizza:
+                        keyword_inner_match = False
+                        break
+
+                if keyword_inner_match:
+                    weekly_menu[day] = pizza
+                    keyword_match = True
+                    break
+        return {
+            "month": month,
+            "startday": int(startday),
+            "endday": int(endday),
+            "menu": weekly_menu
+        }
+
+    def alert_if(self, parsed=None):
+        [month, day] = date.today().strftime("%B %d").split(" ")
+        return parsed["month"] == month and int(day) in range(parsed["startday"], parsed["endday"]+1) and len([d for d in parsed['menu'].keys() if parsed['menu'][d]]) > 0
+
+    def alert_message(self, parsed=None):
+        menu = parsed['menu']
+        days = []
+        for day in menu.keys():
+            if menu[day]:
+                days.append(day)
+        v = []
+        for d in days:
+            v.append((d, menu[d]))
+
+        return [f"{day} - {pizza}" for day, pizza in v]
+
+
 if __name__ == "__main__":
-    sf = StandardFareMonitor(requests.Session(), keywords=["pork", "beet", "beets", "roast beef", "roastbeef", "sausage"])
-    sf.process()
+    MAX_WORKERS = 10
+
+    monitors = [
+        StandardFareMonitor(requests.Session(), keywords=["pork", "beet", "beets", "roast beef", "roastbeef", "sausage"]),
+        ArizmendiMonitor(requests.Session(), keywords=[
+            ["roasted yellow potato", "leek", "parmesan", "garlic oil"],
+        ])
+    ]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        executor.map(lambda m: m.process(), monitors)
